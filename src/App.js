4697131c7fde281d18e0e557f922888190ee6bd7
yatin2123 <yatinv807@gmail.com>
2023-09-05 16:33:52 +0530

    component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Folder Structure
+          Learn React Folder component
         </a>
       </header>
     </div>
