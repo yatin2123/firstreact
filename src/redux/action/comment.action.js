fffa0949971d62b92893efe985e90b3e5e41d3c5
yatin2123 <yatinv807@gmail.com>
2024-01-16 09:29:22 +0530

    done

--- src/redux/action/comment.action.js
+++ src/redux/action/comment.action.js
@@ -1,37 +1,48 @@
 import { useEffect } from "react";
-import { ADD_COMMENT, GET_COMMENT } from "../ActionType"
+import { ADD_COMMENT, DELETE_COMMENT, ERROR_COMMENT, GET_COMMENT, LODING_COMMENT } from "../ActionType"
 
-export const getData =  () =>  async (dispatch) => {
-    try{
+
+const lodingcomment = () => (dispatch) => {
+    dispatch({ type: LODING_COMMENT })
+
+}
+
+const errorcomment = (error) => (dispatch) => {
+    dispatch({type: ERROR_COMMENT, payload: error})
+}
+
+export const getData = () => async (dispatch) => {
+    dispatch(lodingcomment())
+
+    setTimeout(async () => {
+        try {
         console.log('yyyyyyyyyy');
-        const  response = await fetch("https://jsonplaceholder.typicode.com/comments")
-        const  data = await response.json()
+        const response = await fetch("https://jsonplaceholder.typicode.com/comments")
+        const data = await response.json()
         // console.log(data);
 
-        dispatch({type: GET_COMMENT, payload: data})
+        dispatch({ type: GET_COMMENT, payload: data })
+        } catch(error){
+            dispatch(errorcomment(error))
+        }
+    }, 2000)
 
-        
-    } catch(e){
-        console.log(e);
-    }
-   
 }
 
-export const addData =  (data) =>  async (dispatch) => {
+export const addData = (data) => async (dispatch) => {
     console.log(data);
-    try{
-      
-
-      dispatch({type: ADD_COMMENT, payload: data})
-
-        
-    } catch(rejected ){
-        console.log(rejected );
+    try {
+        dispatch({ type: ADD_COMMENT, payload: data })
+    } catch (rejected) {
+        console.log(rejected);
     }
-   
 }
- 
- 
+
+export const deleteData = (id) => (dispatch) => {
+        dispatch({type: DELETE_COMMENT, payload: id})
+}
+
+
 
 
 
--- src/redux/reducer/comment.reducer.js
+++ src/redux/reducer/comment.reducer.js
@@ -1,29 +1,49 @@
-import { ADD_COMMENT, GET_COMMENT } from "../ActionType";
+import { ADD_COMMENT, DELETE_COMMENT, ERROR_COMMENT, GET_COMMENT, LODING_COMMENT } from "../ActionType";
 
 
 const initialState = {
-    isLoding : false,
+    isLoding: false,
     comment: [],
     error: null,
 }
 
-export const commentReducer = (state =initialState, action ) => {
+export const commentReducer = (state = initialState, action) => {
     console.log(action);
 
     switch (action.type) {
+        case LODING_COMMENT:
+            return {
+                isLoding: true,
+                comment: [],
+                error: null,
+            }
+        
+        case ERROR_COMMENT:
+            return{
+                isLoding: false,
+                comment: [],
+                error: action.payload,
+            }
         case GET_COMMENT:
             return {
-                ...state,
-                comment : action.payload
+                isLoding: false,
+                comment: action.payload,
+                error: null,
             }
         case ADD_COMMENT:
-            return{
+            return {
                 ...state,
-                comment : state.comment.concat(action.payload)
+                comment: state.comment.concat(action.payload)
             }
-            
+        
+        case DELETE_COMMENT:
+            return {
+                ...state,
+                comment: state.comment.filter((v) => v.id !== action.payload)
+            }
+
         default:
-           return state;
+            return state;
     }
 
 }
